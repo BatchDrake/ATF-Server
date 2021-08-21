22aa2c1903e852197f45319ad5e2d7d2e0eedaec
Nirro <nireiorange@gmail.com>
2021-08-21 19:18:28 +0200

    Fix reactive views

--- client/src/components/common/Victories/index.jsx
+++ client/src/components/common/Victories/index.jsx
@@ -6,7 +6,8 @@ import { faStar } from "@fortawesome/free-solid-svg-icons";
 function Victories({ amount }) {
   return (
     <div className="victories">
-      {Array(amount).fill(<Icon className="victories__star" icon={faStar} />)}
+      {amount > 1 && <span className="victories__amount">{amount}</span>}
+      <Icon className="victories__star" icon={faStar} />
     </div>
   );
 }
--- client/src/components/ranking/RankingEntry/index.jsx
+++ client/src/components/ranking/RankingEntry/index.jsx
@@ -2,7 +2,7 @@ import "./index.css";
 
 import React from "react";
 import { FontAwesomeIcon as Icon } from "@fortawesome/react-fontawesome";
-import { Col, Row } from "react-bootstrap";
+import { Badge, Col, Row } from "react-bootstrap";
 import { faArrowDown, faArrowUp } from "@fortawesome/free-solid-svg-icons";
 
 import Victories from "../../common/Victories";
@@ -28,21 +28,30 @@ function RankingEntry({ rank }) {
 
   return (
     <Row className="ranking__entry">
-      <Col className="ranking-position">
-        <span className="ranking-position__text">{position}</span>
+      <Col className="ranking-position align-self-center" xs={1}>
+        <Badge pill bg="secondary">
+          {position}
+        </Badge>
       </Col>
-      <Col className="ranking-user">
+      <Col className="d-none d-md-block align-self-center">
         <UserAvatar user={holder} />
+        <span className="ranking-user__name">{holder.name}</span>
+      </Col>
+      <Col className="d-md-none align-self-center">
+        <UserAvatar user={holder} />
+      </Col>
+      <Col className="d-md-none align-self-center">
         <span>{holder.name}</span>
       </Col>
-      <Col className="ranking-separator" xs={1} md={5}></Col>
-      <Col className="ranking-shift">
-        <Shift shift={variation} />
-      </Col>
-      <Col>
+      <Col className="d-none d-sm-block align-self-center" sm={1}>
         <Victories amount={holder.victories} />
       </Col>
-      <Col className="ranking-score">{holder.score}</Col>
+      <Col className="ranking-score align-self-center" xs={1}>
+        {holder.score}
+      </Col>
+      <Col className="ranking-shift align-self-center" xs={2} md={1}>
+        <Shift shift={variation} />
+      </Col>
     </Row>
   );
 }
